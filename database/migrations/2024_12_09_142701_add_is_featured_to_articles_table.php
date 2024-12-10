b5065e4037818812b02299ff323908a8ee1068d5
kameellss <161406681+kameellss@users.noreply.github.com>
2024-12-10 20:59:05 +0700

    Create 2024_12_09_142701_add_is_featured_to_articles_table.php

---
+++ database/migrations/2024_12_09_142701_add_is_featured_to_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->boolean('is_featured')->default(false); // Menambahkan kolom
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropColumn('is_featured'); // Menghapus kolom jika rollback
+        });
+    }
+};
