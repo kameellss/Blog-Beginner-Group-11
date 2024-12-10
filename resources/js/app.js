d7497087a24b5bbd06c42238e68f147709dd08d2
kameellss <161406681+kameellss@users.noreply.github.com>
2024-12-10 20:59:36 +0700

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,4 @@
 import './bootstrap';
 
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
