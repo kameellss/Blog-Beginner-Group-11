aa378f6fa374c484964da9618b86a6dadce4af14
kameellss <161406681+kameellss@users.noreply.github.com>
2024-12-10 20:59:22 +0700

    Create UserSeeder.php

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+
+}
